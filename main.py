e3a959cc6a8f9c49ac3b7411a576382ffee0d75e
Dmitry7007 <99890690+Dmitry7007@users.noreply.github.com>
2024-09-19 13:39:42 +0700

    19.09.2024

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# @app.get("/")
+# def read_root():
+# return {"Hello World"}
 
-@app.get("/")
-def read_root():
-    return {"Hello World"}
 
+class RegistrationRequest(BaseModel):
+    id: int
+    username: str
+    emal: Union[str, None] = None
+    password: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/registration")
+async def registration(item: RegistrationRequest):
+    return {
+        "message": "User registered successfully"
+    }
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@app.post("/login")
+async def login(item: LoginRequest):
+    if not all([
+        len(item.password) >= 8,
+        any(char.isdigit() for char in item.password),
+        any(char.isupper() for char in item.password),
+    ]):
+        return {
+            "message": "Password must be at least 8 characters, \
+             contain at least one digit and one uppercase letter"
+        }
+    return {
+        "message": "Login successful"
+    }
